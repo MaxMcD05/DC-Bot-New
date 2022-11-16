25b9c6c538db42daa7752fa2b8b4a4aa38fe9708
MaxMcD05 <mm8564a@student.american.edu>
2022-11-16 13:24:09 -0500

    w

--- index.js
+++ index.js
@@ -111,19 +111,6 @@ client.on('interactionCreate', async interaction => {
 
 	client.on('message', message => {
 
-    if (!interaction.isChatInputCommand()) return;
-
-    const command = client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-    }
-});
 });
 
 client.login(token);
